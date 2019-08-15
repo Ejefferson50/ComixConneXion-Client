14bdf6ba7024fe9f9c246e34d845e138d28fe6d4
Eric <ejefferson50@gmail.com>
2019-08-15 12:47:34 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponentComponent } from './component/user-component/user-component.component';
+import { LoginComponentComponent } from './component/login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
