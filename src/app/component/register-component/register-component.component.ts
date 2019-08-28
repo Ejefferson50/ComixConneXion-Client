73b8c2662001a7f46560b3089d44c9e5e5f90473
Eric <ejefferson50@gmail.com>
2019-08-28 18:27:34 -0400

    Worked on password validator

--- src/app/component/register-component/register-component.component.ts
+++ src/app/component/register-component/register-component.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../models/User';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {PasswordValidator} from '../validators/password.validator';
 
 @Component({
@@ -16,12 +16,19 @@ export class RegisterComponentComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = this.fb.group({
-      email: [''],
-      username: [''],
+      email: ['', Validators.required],
+      userName: ['', Validators.required],
       password: [''],
       confirmPassword: ['']
     }, {validator: PasswordValidator}
     );
+  }
+  get userName() {
+    return this.registerForm.get('userName');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
   }
 
 }
