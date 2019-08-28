02d81417e46a751e48bdc551c1bb59dcd6a0af95
Eric <ejefferson50@gmail.com>
2019-08-28 17:45:45 -0400

    Working on Registration

--- src/app/register-component/register-component.component.ts
+++ src/app/register-component/register-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../models/User';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-register-component',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-component.component.css']
 })
 export class RegisterComponentComponent implements OnInit {
+  user: User;
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.fb.group({
+      email: [''],
+      username: [''],
+      password: [''],
+      confirmPassword: ['']
+    });
   }
 
 }
